96abe3883779b9240a4055415eab861ab21b224e
tired6488 <85363080+tired6488@users.noreply.github.com>
2021-07-28 17:12:38 -0500

    updating nano to trollar in a few places

--- src/app/partials/explorer/frontiers/DistributionStats.js
+++ src/app/partials/explorer/frontiers/DistributionStats.js
@@ -18,7 +18,7 @@ export default function DistributionStats({ distribution }) {
 
 const DistributionRange = ({ range, value }) => (
   <div className="col-md col-6 text-center mb-1">
-    <p className="text-muted mb-1">{range} NANO</p>
+    <p className="text-muted mb-1">{range} TROLLAR</p>
     <h4>
       <FormattedNumber value={value || 0} />
     </h4>
